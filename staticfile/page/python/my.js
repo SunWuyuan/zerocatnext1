4321675c4e84f7d58c2d773bc482241aa0bb3872
SunWuyuan <1847261658@qq.com>
2024-07-08 20:29:44 +0800

    1

---
+++ staticfile/page/python/my.js
@@ -0,0 +1,184 @@
+//翻页加载作品
+var _laypage = layui.laypage;
+
+function loadPage(count, state) {
+
+  _laypage.render({
+    elem: "layui_change_page",
+    count: count,
+    limit: 16,
+    theme: "#3593ff",
+    layout: ["count", "prev", "page", "next", "limit", "refresh", "skip"],
+    limits: [8, 16, 32],
+    curr: 1,
+    jump: function (obj, first) {
+      AjaxGet("/my/getPythonProjects",{ curr: obj.curr, limit: obj.limit, state: state },function (d) {
+        if (d.length) {
+          $("#box_projects").html("");
+          pythoninfo = d;
+          if (state == 0) {
+            for (var i = 0; i < d.length; i++) {
+              $("#box_projects").append(`
+
+
+
+
+    <div class="mdui-col-xl-2 mdui-col-lg-2 mdui-col-md-3 mdui-col-sm-6 mdui-col-xs-12" style="margin:5px 0px 5px 0px;">
+        <mdui-card variant="outlined" clickable ondragstart="return false" style="user-select:none;width: 100%;overflow: hidden">
+          <div class="card-main">
+              <div class="substr card-main-text" >${d[i].title}</div>
+              <div class="substr card-main-subtitle">${d[i].description}[${d[i].view_count}浏览]</div>
+            </div>
+          <div style="padding: 16px;">
+            <mdui-button onclick="delProject(this,${d[i].id})">删除</mdui-button>
+            <mdui-button onclick="shareProject(${d[i].id})">分享</mdui-button>
+            <mdui-button onclick="SetProjectDescription(${i})">简介</mdui-button>
+            <mdui-button onclick='location.href=("/python/edit#${d[i].id}")'>编辑</mdui-button>
+          </div>
+        </mdui-card>
+        </div>
+    `);
+            }
+          } else {
+            for (var i = 0; i < d.length; i++) {
+              $("#box_projects")
+                .append(`<div class="mdui-col-xl-2 mdui-col-lg-2 mdui-col-md-3 mdui-col-sm-6 mdui-col-xs-12" style="margin:5px 0px 5px 0px;">
+        <mdui-card variant="outlined" clickable ondragstart="return false" style="user-select:none;width: 100%;overflow: hidden">
+          <div class="card-main">
+              <div class="substr card-main-text" >${d[i].title}</div>
+              <div class="substr card-main-subtitle">${d[i].description}[${d[i].view_count}浏览]</div>
+            </div>
+          <div style="padding: 16px;">
+            <mdui-button onclick="delProject(this,${d[i].id})">删除</mdui-button>
+            <mdui-button onclick="noshareProject(${d[i].id})">取消分享</mdui-button>
+            <mdui-button onclick="SetProjectDescription(${i})">简介</mdui-button>
+            <mdui-button onclick='location.href=("/python/edit#${d[i].id}")'>编辑</mdui-button>
+          </div>
+        </mdui-card>
+        </div>
+
+    `);
+            }
+          }
+        } else {
+          $("#box_projects").append(
+            `<mdui-button href='/python/edit'>创建作品</mdui-button>`
+          );
+        }
+      })
+
+    },
+  });
+}
+
+//未分享的作品
+function project_state0() {
+  $("#box_projects").html("");
+  loadPage(state0_count, 0);
+}
+//已分享的作品
+function project_state1() {
+  $("#box_projects").html("");
+  loadPage(state1_count, 1);
+}
+//已开源的作品
+function project_state2() {
+  $("#box_projects").html("");
+  loadPage(state2_count, 2);
+}
+
+
+$(function () {
+  getinfo(function() {
+    project_state0()  });
+});
+var state0_count = 0
+var state1_count = 0
+var state2_count = 0
+function getinfo(load) {
+  AjaxGet("/api/myprojectcount?type=python", {}, function (data) {
+    console.log("成功获取项目数量信息");
+    console.log(data);
+    state0_count = data.state0_count
+    state1_count = data.state1_count
+    state2_count = data.state2_count
+    load()
+
+      });
+
+}
+
+//分享作品
+function shareProject(id) {
+  mdui.confirm({
+    headline: "确认分享作品?",
+    description: "请确认操作",
+    confirmText: "确认",
+    cancelText: "取消",
+    onConfirm: () =>
+      AjaxFn("/my/python/share", { id: id }, function (res) {
+        automsg(res["msg"]);
+        if ("success" == res["status"]) {
+          window.location.reload();
+        }
+      }),
+  });
+}
+function noshareProject(id) {
+  mdui.confirm({
+    headline: "确认取消分享作品?",
+    description: "请确认操作",
+    confirmText: "确认",
+    cancelText: "取消",
+    onConfirm: () =>
+      AjaxFn("/my/python/noshare", { id: id }, function (res) {
+        automsg(res["msg"]);
+        if ("success" == res["status"]) {
+          window.location.reload();
+        }
+      }),
+  });
+}
+
+function SetProjectDescription(id) {
+  mdui.prompt({
+    headline: "简介",
+    description: "请输入简介",
+    confirmText: "确认",
+    cancelText: "取消",
+    textFieldOptions: {
+      value: pythoninfo[id].description || "null",
+      autosize: "1",
+      maxlength: "1000",
+      counter: "1",
+    },
+    onConfirm: (value) =>
+      AjaxFn(
+        "/my/python/setdescription",
+        { id: pythoninfo[id].id, description: value },
+        function (res) {
+          automsg(res["msg"]);
+          if ("success" == res["status"]) {
+            window.location.reload();
+          }
+        }
+      ),
+    onCancel: () => console.log("canceled"),
+  });
+}
+//删除作品
+function delProject(that, id) {
+  mdui.confirm({
+    headline: "确认删除作品?",
+    description: "请确认操作",
+    confirmText: "确认",
+    cancelText: "取消",
+    onConfirm: () =>
+      AjaxFn("/my/python/del", { id: id }, function (res) {
+        automsg(res["msg"]);
+        if ("success" == res["status"]) {
+          window.location.reload();
+        }
+      }),
+  });
+}
---
+++ staticfile/page/scratch/playpage.js
@@ -0,0 +1,203 @@
+_pid = getQueryString('id');
+
+socialShare(".social-share", $config);
+function projectanalysis() {
+  var chartDom = document.getElementById("main1");
+  var myChart = echarts.init(chartDom);
+  var option;
+  var h = {
+    context: getHead(JSON.parse(projectjson)),
+    info: "<%= project.author_display_name %> 的作品 <%= project.title %> 的分析",
+    infotext: getStats(JSON.parse(projectjson)),
+  };
+  console.log(h);
+  var opcodes = {
+    looks: "外观",
+    control: "控制",
+    motion: "移动",
+    sound: "声音",
+    event: "事件",
+    sensing: "侦测",
+    operator: "运算",
+    procedures: "自定义",
+    argument: "参数",
+    data: "数据",
+    pen: "画笔",
+    other: "其它",
+  };
+  const data = genData(50);
+  option = {
+    tooltip: {
+      trigger: "item",
+      formatter: "{a} <br/>{b} : {c} ({d}%)",
+    },
+    legend: {
+      type: "scroll",
+      orient: "vertical",
+      right: 10,
+      top: 10,
+      bottom: 10,
+      data: data.legendData,
+    },
+    series: [
+      {
+        name: "类别",
+        type: "pie",
+        radius: "55%",
+        center: ["40%", "50%"],
+        data: data.seriesData,
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: "rgba(0, 0, 0, 0.5)",
+          },
+        },
+      },
+    ],
+  };
+  function genData(count) {
+    // prettier-ignore
+    const legendData = [];
+    const seriesData = [];
+    let o = Object.keys(h.context),
+      s = 0;
+    for (var i = 0; i < o.length; i++) {
+      s += h.context[o[i]];
+    }
+    for (var i = 0; i < o.length; i++) {
+      var name = opcodes[o[i]] || o[i];
+      legendData.push(name);
+      seriesData.push({
+        name: name + "  " + ((h.context[o[i]] / s) * 100).toFixed(1) + "%",
+        value: h.context[o[i]],
+      });
+    }
+    var infotext = h.infotext;
+    console.log(infotext);
+    document.getElementById("r").innerHTML =
+      // "不含圆形积木数:"+h.noreturn+"<br>"+
+      "<mdui-chip>变量数:" +
+      infotext.variables +
+      "个</mdui-chip>" +
+      "<mdui-chip>列表数:" +
+      infotext.lists +
+      "个</mdui-chip>" +
+      "<mdui-chip>广播数:" +
+      infotext.broadcasts +
+      "个</mdui-chip>" +
+      "<mdui-chip>总块数:" +
+      infotext.blocks +
+      "块</mdui-chip>" +
+      "<mdui-chip>插件数:" +
+      infotext.extensions +
+      "个</mdui-chip>";
+    return {
+      legendData: legendData,
+      seriesData: seriesData,
+      total: s,
+    };
+  }
+
+  option && myChart.setOption(option);
+}
+function FormatTime(t, date) {
+  var date = new Date(date);
+  var o = {
+    "M+": date.getMonth() + 1, //月份
+    "d+": date.getDate(), //日
+    "h+": date.getHours(), //小时
+    "m+": date.getMinutes(), //分
+    "s+": date.getSeconds(), //秒
+    "q+": Math.floor((date.getMonth() + 3) / 3), //季度
+    S: date.getMilliseconds(), //毫秒
+  };
+  if (/(y+)/.test(t)) {
+    t = t.replace(
+      RegExp.$1,
+      (date.getFullYear() + "").substr(4 - RegExp.$1.length)
+    );
+  }
+  for (var k in o) {
+    if (new RegExp("(" + k + ")").test(t)) {
+      t = t.replace(
+        RegExp.$1,
+        RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length)
+      );
+    }
+  }
+  return t;
+}
+function getQueryString(name) {
+  const url_string = window.location.href;
+  const url = new URL(url_string);
+  return url.searchParams.get(name);
+}
+var projectinfo={}
+var $config = {
+  title: 'ZeroCat社区',
+  description: 'ZeroCat社区',
+  image: S3staticurl+'/scratch_slt/1',
+  wechatqrcodeTitle: "微信扫一扫分享",
+  wechatqrcodeHelper: '<p>打开扫一扫</p><p>在手机上打开即可分享至朋友圈。</p>',
+};
+function getprojectinfo() {
+  _pid = getQueryString('id');
+  AjaxGet("/scratch/projectinfo?id="+ getQueryString("id"), {}, function (result) {
+    projectinfo = result
+    document.querySelector("#project_iframe").src = editorurl+'/embed.html#'+result.id;
+  document.querySelector("#authorinfo").headline =
+        result.author_display_name;
+      document.querySelector("#authorinfo").description = result.author_motto;
+      document.querySelector("#authorinfo").href = "/user.html?id=" + result.authorid;
+      document.querySelector("#editlink-old").href = "/scratch/edit#" + result.id;
+      if (Cookies.get('token')) {
+        document.querySelector("#editlink").href = editorurl+"/editor.html?token="+ Cookies.get('token') +'#' + result.id;
+      }else{
+        document.querySelector("#editlink").href = editorurl+"/editor.html#" + result.id;
+
+      }
+
+      document.querySelector("#authoravatar").src =
+        S3staticurl + "/user/" + result.author_images;
+      document.querySelector("#projecttitle").innerText = result.title;
+      document.querySelector("#projectdescription").innerText =
+        result.description;
+      document.querySelector("#projecttime").innerText = `最后更新:${FormatTime(
+        "yyyy-MM-dd hh:mm:ss",
+        result.time
+      )}`;
+      document.querySelector("#projectview").innerText =
+        result.view_count + "浏览";
+      if (result.state == "0") {
+        document
+          .querySelector("#projectstate")
+          .setAttribute("icon", "lock_person");
+        document.querySelector("#projectstate").innerText = "未分享";
+      } else if (result.state == "1") {
+        document.querySelector("#projectstate").setAttribute("icon", "share");
+        document.querySelector("#projectstate").innerText = "公开作品";
+      } else if (result.state == "2") {
+        document.querySelector("#projectstate").setAttribute("icon", "star");
+        document.querySelector("#projectstate").innerText = "优秀作品";
+      }
+      like_count = result.like_count
+      favo_count = result.favo_count
+      $config = {
+				title: result.title,
+				description: `${result.title}是${result.author_display_name}在ZeroCat社区上的作品`,
+				image: S3staticurl+'/scratch_slt/'+result.id,
+				wechatqrcodeTitle: "微信扫一扫分享",
+				wechatqrcodeHelper: '<p>打开扫一扫</p><p>在手机上打开即可分享至朋友圈。</p>',
+			};
+      console.log("成功获取作品信息");
+      console.log(result);
+
+      $(".remove-disabled").removeAttr("disabled");
+     });
+
+
+}
+$(function () {
+  getprojectinfo();
+});
