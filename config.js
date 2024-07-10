a33d39c6e01a1f02c6a15980409fccd630240701
SunWuyuan <1847261658@qq.com>
2024-07-10 15:33:48 +0800

    1

---
+++ config.js
@@ -0,0 +1,10 @@
+  var config = {
+    "s3url": "https://s4-1.wuyuan.1r.ink",
+    "editorurl": "https://zerocat-static.houlangs.com/scratchguifile",
+    "server": "https://zerocat.houlangs.com"
+  }
+window.config=config
+staticurl=config.s3url
+Ow_Server=config.server
+staticurl='/static'
+editorurl=config.editorurl
